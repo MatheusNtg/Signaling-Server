3335d3a02299a1e605139a522554ec9dc6bb0006
Matheus <matheus@ufg.br>
2019-06-15 20:37:18 -0300

    Resolução de conflitos

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // Const
 const PORT = 7000;
-const NEW_ICE_CANDIDATE = 'newIceCandidate';
-const SDP               = 'sdp';
-const ANSWER            = 'answer';
+const MESSAGE  = "message";
+const SET_ID   = "set_id"
 
 // Vars to create the server
 const app  = require('express')();
@@ -10,52 +9,34 @@ const http = require('http').createServer(app);
 const io   = require('socket.io')(http);
 
 // The list of sockets connected
-var peers = [];
+var users = new Map();
 
 // This is the function that is triggered when someone connect to the server
 io.on('connection', socket => {
   console.log(`Someone has connected to the server`);
+  users.set(socket.id,null);
+  socket.emit(SET_ID, "set_id");
+
+  socket.on(MESSAGE,msg => {
+    users.set(socket.id,msg);
+    console.log(users);
+    makeTheCall(msg.to,msg);
+  });
+
+  socket.on("RESPONSE", msg => {
+    console.dir(msg);
+    users.set(socket.id, msg.username);
+  });
   
-  // Handle with the register of new users
-  // socket.on('registration', user_received => {
-  //   let user = {
-  //     name: JSON.parse(user_received).name,
-  //     id  : socket.id,
-  //     type: JSON.parse(user_received).type
-  //   }
-  //   switch(user.type){
-  //     case "monitor":
-  //       console.log("A new monitor has been registered");
-  //       monitors_connected.push(user);
-  //       break;
-  //     case "locator":
-  //       console.log("A new locator has been registered");
-  //       locators_connected.push(user);
-  //       break;
-  //     default:
-  //       console.log("There was an error with the register of a new user");  
-  //   }
-  // });
-
-  socket.on(NEW_ICE_CANDIDATE, msg =>{
-    console.log(msg);
-  });
-
-  socket.on(SDP, msg => {
-    console.log("Received a SDP message from a client");
-    let object = JSON.parse(msg);
-    io.emit(ANSWER,object);
-  });
-
   // Handle when a user disconnected
   socket.on('disconnect', () => {
     // This looks if a user that's disconneting is a monitor or a locator then removes it from the proper array
-    
+  })
 });
 
 
 // Put the server listening on PORT
-http.listen(PORT, () =>{
+http.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
 
@@ -64,4 +45,25 @@ function printActiveClients(){
   users_connected.forEach(element => {
     console.log(element.id);
   }); 
+}
+
+function makeTheCall(username, msg){
+  let calleid = getSocketIdByUserName(username);
+  io.to(`${calleid}`).emit(MESSAGE,msg); 
+}
+
+function getSocketIdByUserName(username){
+  let result;
+  for(const [key,value] of users){
+    if(typeof value === "object"){
+      if(value.username === username){
+        result = key;
+      }
+    }else{
+      if(value === username){
+        result = key;
+      }
+    }
+  }
+  return result;
 }
